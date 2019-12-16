94a434ab8edb19dd55ef73ea1c466f7f747a7a14
holyxlat <35751413+holyxlat@users.noreply.github.com>
2019-12-16 16:28:56 +0900

    Update ProductController.java

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -25,14 +25,6 @@ public class ProductController {
     @GetMapping("/product/{productId}")
     Product productStockCheck2(@PathVariable(value = "productId") Long productId) {
 
-        System.out.println("productStockCheck call");
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return  this.productService.getProductById(productId);
     }
     
     @PostMapping("/product")
